378f2afa8a0877b57953eafbf01e1e3534be6d41
Defne <defneecey@gmail.com>
2024-11-26 13:50:57 +0000

    Test get featured playlists function

--- code/functions.py
+++ code/functions.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 def search_artist(artist_name, token):
     url = "https://api.spotify.com/v1/search"
@@ -31,3 +32,34 @@ def get_target_market(target_markets, token):
     }
    response = requests.get(url, headers=headers, params=params)
    return response.json()
+
+
+def get_featured_playlists(token, country, locale=None, limit=10):
+    url = "https://api.spotify.com/v1/browse/featured-playlists"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    params = {
+        "country": country,
+        "locale": locale,
+        "limit": limit
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status() 
+
+    data = response.json()
+    playlists = data['playlists']['items']
+    
+    playlist_data = []
+    for playlist in playlists:
+        playlist_data.append({
+            "playlist_id": playlist['id'],
+            "playlist_name": playlist['name'],
+            "description": playlist['description'],
+            "owner": playlist['owner']['display_name'],
+            "num_tracks": playlist['tracks']['total'],
+            "url": playlist['external_urls']['spotify']
+        })
+    
+    return pd.DataFrame(playlist_data)

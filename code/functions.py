4b08d37127da1eafed41758c7487c00d845ae929
Defne <defneecey@gmail.com>
2024-11-26 11:04:05 +0000

    Test get_market function

--- code/functions.py
+++ code/functions.py
@@ -14,3 +14,17 @@ def search_artist(artist_name, token):
     response = requests.get(url, headers=headers, params=params)
     return response.json()
 
+
+def get_market(markets, token):
+    url = "https://api.spotify.com/v1/markets"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    params = {
+        "q": markets,
+        "type": "market",
+        "limit": 1
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
